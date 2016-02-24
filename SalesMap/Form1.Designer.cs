25e6226f3d4e20641841f91a6ecb01a8f471802c
derekantrican <derekantrican@gmail.com>
2016-02-24 09:12:27 -0800

    First commit (currently missing Sales Rep contact info, their responsible regions, pictures of regions, and the settings dialog)

---
+++ SalesMap/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SalesMap
+{
+    partial class SalesMapSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelState = new System.Windows.Forms.Label();
+            this.labelOR = new System.Windows.Forms.Label();
+            this.labelRepresentative = new System.Windows.Forms.Label();
+            this.pictureBackground = new System.Windows.Forms.PictureBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonMap = new System.Windows.Forms.Button();
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
+            this.comboBoxRepresentative = new System.Windows.Forms.ComboBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.labelRepResult = new System.Windows.Forms.Label();
+            this.labelRegionResult = new System.Windows.Forms.Label();
+            this.labelContactResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 184);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(371, 323);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.labelState.Location = new System.Drawing.Point(21, 29);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(82, 13);
+            this.labelState.TabIndex = 3;
+            this.labelState.Text = "State/Province:";
+            // 
+            // labelOR
+            // 
+            this.labelOR.AutoSize = true;
+            this.labelOR.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.labelOR.Location = new System.Drawing.Point(77, 49);
+            this.labelOR.Name = "labelOR";
+            this.labelOR.Size = new System.Drawing.Size(41, 13);
+            this.labelOR.TabIndex = 5;
+            this.labelOR.Text = "-- OR --";
+            // 
+            // labelRepresentative
+            // 
+            this.labelRepresentative.AutoSize = true;
+            this.labelRepresentative.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.labelRepresentative.Location = new System.Drawing.Point(21, 71);
+            this.labelRepresentative.Name = "labelRepresentative";
+            this.labelRepresentative.Size = new System.Drawing.Size(90, 13);
+            this.labelRepresentative.TabIndex = 6;
+            this.labelRepresentative.Text = "Sales Rep Name:";
+            // 
+            // pictureBackground
+            // 
+            this.pictureBackground.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.pictureBackground.Location = new System.Drawing.Point(12, 12);
+            this.pictureBackground.Name = "pictureBackground";
+            this.pictureBackground.Size = new System.Drawing.Size(263, 95);
+            this.pictureBackground.TabIndex = 8;
+            this.pictureBackground.TabStop = false;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(295, 104);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 9;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // buttonMap
+            // 
+            this.buttonMap.Location = new System.Drawing.Point(295, 75);
+            this.buttonMap.Name = "buttonMap";
+            this.buttonMap.Size = new System.Drawing.Size(75, 23);
+            this.buttonMap.TabIndex = 10;
+            this.buttonMap.Text = "Open Map";
+            this.buttonMap.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Location = new System.Drawing.Point(109, 26);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(151, 21);
+            this.comboBoxState.TabIndex = 11;
+            this.comboBoxState.SelectedIndexChanged += new System.EventHandler(this.comboBoxState_SelectedIndexChanged);
+            // 
+            // comboBoxRepresentative
+            // 
+            this.comboBoxRepresentative.FormattingEnabled = true;
+            this.comboBoxRepresentative.Location = new System.Drawing.Point(109, 68);
+            this.comboBoxRepresentative.Name = "comboBoxRepresentative";
+            this.comboBoxRepresentative.Size = new System.Drawing.Size(151, 21);
+            this.comboBoxRepresentative.TabIndex = 12;
+            this.comboBoxRepresentative.SelectedIndexChanged += new System.EventHandler(this.comboBoxRepresentative_SelectedIndexChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SalesMap.Properties.Resources.settings;
+            this.pictureBox2.Location = new System.Drawing.Point(369, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // labelRepResult
+            // 
+            this.labelRepResult.AutoSize = true;
+            this.labelRepResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRepResult.Location = new System.Drawing.Point(12, 121);
+            this.labelRepResult.Name = "labelRepResult";
+            this.labelRepResult.Size = new System.Drawing.Size(75, 16);
+            this.labelRepResult.TabIndex = 14;
+            this.labelRepResult.Text = "Sales Rep:";
+            // 
+            // labelRegionResult
+            // 
+            this.labelRegionResult.AutoSize = true;
+            this.labelRegionResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRegionResult.Location = new System.Drawing.Point(12, 162);
+            this.labelRegionResult.Name = "labelRegionResult";
+            this.labelRegionResult.Size = new System.Drawing.Size(55, 16);
+            this.labelRegionResult.TabIndex = 15;
+            this.labelRegionResult.Text = "Region:";
+            // 
+            // labelContactResult
+            // 
+            this.labelContactResult.AutoSize = true;
+            this.labelContactResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContactResult.Location = new System.Drawing.Point(12, 141);
+            this.labelContactResult.Name = "labelContactResult";
+            this.labelContactResult.Size = new System.Drawing.Size(56, 16);
+            this.labelContactResult.TabIndex = 16;
+            this.labelContactResult.Text = "Contact:";
+            // 
+            // SalesMapSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 515);
+            this.Controls.Add(this.labelContactResult);
+            this.Controls.Add(this.labelRegionResult);
+            this.Controls.Add(this.labelRepResult);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.comboBoxRepresentative);
+            this.Controls.Add(this.comboBoxState);
+            this.Controls.Add(this.buttonMap);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.labelRepresentative);
+            this.Controls.Add(this.labelOR);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBackground);
+            this.Name = "SalesMapSearch";
+            this.Text = "Sales Map Search";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBackground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Label labelOR;
+        private System.Windows.Forms.Label labelRepresentative;
+        private System.Windows.Forms.PictureBox pictureBackground;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonMap;
+        private System.Windows.Forms.ComboBox comboBoxState;
+        private System.Windows.Forms.ComboBox comboBoxRepresentative;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label labelRepResult;
+        private System.Windows.Forms.Label labelRegionResult;
+        private System.Windows.Forms.Label labelContactResult;
+    }
+}
+
---
+++ SalesMap/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesMap
+{
+    public partial class SalesMapSearch : Form
+    {
+        public SalesMapSearch()
+        {
+            InitializeComponent();
+
+            //Eventual format: [][] [[REGION_NAME, IS_MULTIPART]]
+            string[] regions = new string[] {"","(AL) Alabama", "(AK) Alaska", "(AZ) Arizona","(AR) Arkansas",
+                                            "(BC) British Columbia",
+                                            "(CA) California", "(CO) Colorado", "(CT) Conneticut",
+                                            "(DE) Delaware", "(FL) Florida", "(GA) Georgia", "(HI) Hawaii",
+                                            "(ID) Idaho", "(IL) Illinois", "(IN) Indiana", "(IA) Iowa",
+                                            "(KS) Kansas", "(KY) Kentucky", "(LA) Louisiana", "(ME) Maine", "(MA) Massachusetts",
+                                            "(MB) Manitoba",
+                                            "(MI) Michigan", "(MN) Minnesota", "(MS) Mississippi", "(MO) Missouri", "(MT) Montana",
+                                            "(NB) New Brunswick",
+                                            "(NE) Nebraska", "(NV) Nevada", "(NH) New Hampshire", "(NJ) New Jersey", "(NL) Newfoundland and Labrador",
+                                            "(NM) New Mexico",
+                                            "(NY) New York", "(NC) North Carolina", "(ND) North Dakota", "(OH) Ohio", "(OK) Oklahoma",
+                                            "(OR) Oregon", "(PA) Pennsylvania", "(RI) Rhode Island", "(SC) South Carolina", "(SD) South Dakota",
+                                            "(TN) Tennessee", "(TX) Texas", "(UT) Utah", "(VT) Vermont", "(VA) Virginia", "(WA) Washington!",
+                                            "(WV) West Virginia", "(WI) Wisconsin", "(WY) Wyoming"};
+
+            //Eventual format: [][] [[REP_NAME,EMAIL,PHONE,RESPONSIBLE_REGION_INDEX]]
+            string[] representatives = new string[] { "", "Brian Blair", "Arie Brown", "Matt Brubaker", "Donnie Dugger", "Jim Elmore", "Faber Fields",
+                                                      "Jennifer Galigher", "Joaquin Gonzales", "Andrew Hood", "Dan Jacobs", "Larsen Kjellman",
+                                                      "Scott Lindley", "Ryan Lustig", "Albert Otto", "Jonathan Padial", "David Sipho", "Ethan Swanson",
+                                                      "Scott Taylor", "Steve Ties", "Schalk van Niekerk", "Ryan Weymouth", "Bryan Wurbarton"};
+
+            comboBoxState.DataSource = regions;
+            comboBoxRepresentative.DataSource = representatives;
+        }
+
+        private void comboBoxRepresentative_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxRepresentative.SelectedItem.ToString() != "")
+            {
+                comboBoxState.SelectedIndex = 0;
+                labelRegionResult.Text = "Region: ";
+
+                labelRepResult.Text = "Sales Rep: " + comboBoxRepresentative.SelectedItem.ToString();
+                //labelContactResult.Text = "Contact: " + 
+            }
+
+        }
+
+        private void comboBoxState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxState.SelectedItem.ToString() != "")
+            {
+                comboBoxRepresentative.SelectedIndex = 0;
+                labelRepResult.Text = "Sales Rep: ";
+                labelContactResult.Text = "Contact: ";
+
+                labelRegionResult.Text = "Region: " + comboBoxState.SelectedItem.ToString();
+            }
+        }
+    }
+}
