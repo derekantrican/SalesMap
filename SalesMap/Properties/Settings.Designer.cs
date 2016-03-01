764feaa32515140a647b311f632ddf13e18e765b
derekantrican <derekantrican@gmail.com>
2016-03-01 15:49:59 -0800

    Updated the version number

--- SalesMap/Properties/Settings.Designer.cs
+++ SalesMap/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace SalesMap.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v0.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v0.4")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
