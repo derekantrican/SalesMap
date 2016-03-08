ad6536bd63d75d3dc128c18620e5f633035957b0
derekantrican <derekantrican@gmail.com>
2016-03-08 15:26:27 -0800

    "AutoCheckUpdates" is now "True" by default

--- SalesMap/Properties/Settings.Designer.cs
+++ SalesMap/Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace SalesMap.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AutoCheckUpdate {
             get {
                 return ((bool)(this["AutoCheckUpdate"]));
