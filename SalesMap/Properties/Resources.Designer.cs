6514101d23fb11e1e356fbdc6aaab92cf7c3cd21
derekantrican <derekantrican@gmail.com>
2016-03-23 07:47:49 -0700

    Updated the photo for Texas

--- SalesMap/Properties/Resources.Designer.cs
+++ SalesMap/Properties/Resources.Designer.cs
@@ -922,39 +922,32 @@ namespace SalesMap.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ,
-        ///(AB) Alberta,0
-        ///(AL) Alabama,0
-        ///(AK) Alaska,0
-        ///(AZ) Arizona,0
-        ///(AR) Arkansas,0
-        ///(BC) British Columbia,0
-        ///(CA) California,1
-        ///(CO) Colorado,0
-        ///(CT) Connecticut,0
-        ///(DE) Delaware,0
-        ///(FL) Florida,0
-        ///(GA) Georgia,0
-        ///(HI) Hawaii,0
-        ///(ID) Idaho,0
-        ///(IL) Illinois,0
-        ///(IN) Indiana,1
-        ///(IA) Iowa,0
-        ///(KS) Kansas,0
-        ///(KY) Kentucky,0
-        ///(LA) Louisiana,0
-        ///(ME) Maine,0
-        ///(MA) Massachusetts,0
-        ///(MB) Manitoba,0
-        ///(MD) Maryland,0
-        ///(MI) Michigan,1
-        ///(MN) Minnesota,0
-        ///(MS) Mississippi,0
-        ///(MO) Missouri,0
-        ///(MT) Montana,0
-        ///(NB) New Brunswick,0
-        ///(NE) Nebraska,0
-        ///(NV) Neva [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to ,,
+        ///(AB) Alberta,0,NW
+        ///(AL) Alabama,0,SE
+        ///(AK) Alaska,0,NW
+        ///(AZ) Arizona,0,SW
+        ///(AR) Arkansas,0,N&amp;ST
+        ///(BC) British Columbia,0,NW
+        ///(CA) California,1,NW:SW
+        ///(CO) Colorado,0,NW
+        ///(CT) Connecticut,0,NE
+        ///(DE) Delaware,0,NE
+        ///(FL) Florida,0,SE
+        ///(GA) Georgia,0,SE
+        ///(HI) Hawaii,0,HI
+        ///(ID) Idaho,0,NW
+        ///(IL) Illinois,0,N&amp;ST
+        ///(IN) Indiana,1,GL
+        ///(IA) Iowa,0,N&amp;ST
+        ///(KS) Kansas,0,N&amp;ST
+        ///(KY) Kentucky,0,GL
+        ///(LA) Louisiana,0,SE
+        ///(ME) Maine,0,NE
+        ///(MA) Massachusetts,0,NE
+        ///(MB) Manitoba,0,CND
+        ///(MD) Maryland,0,NE
+        ///(MI) Michigan,1,N&amp;S [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Regions {
             get {
@@ -983,15 +976,15 @@ namespace SalesMap.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to ,,,
-        ///Brian Blair,brianb@sigmanest.com,513-314-7422,BC:AB:WA:MT:AK
-        ///Arie Brown,arieb@sigmanest.com,513-335-7432,GA:LA:TN:AL:MS
-        ///Matt Brubaker,mattb@sigmanest.com,513-532-3544,IN (N):OH (N)
-        ///Jason DeGraeve,jason.degraeve@sigmanest.com,519-831-5774,ON (N):SK:NT:YT:NU
-        ///Donnie Dugger,donnie.dugger@sigmanest.com,816-454-0600,KS:MO
-        ///Jim Elmore,jim.elmore@sigmanest.com,513-214-9269,CA (S):NV(S):AZ
-        ///Faber Fields,faber.fields@sigmanest.com,513-430-2973,IN (S)
-        ///Jennifer Gallagher,jennifer.gallagher@sigmanest.com,847-420-7235, [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to ,,,,
+        ///Brian Blair,brianb@sigmanest.com,513-314-7422,BC:AB:WA:MT:AK,RSM:NW
+        ///Arie Brown,arieb@sigmanest.com,513-335-7432,GA:LA:TN:AL:MS,RSM:SE
+        ///Matt Brubaker,mattb@sigmanest.com,513-532-3544,IN (N):OH (N),RSM:GL
+        ///Jason DeGraeve,jason.degraeve@sigmanest.com,519-831-5774,ON (N):SK:NT:YT:NU,RSM:CND
+        ///Donnie Dugger,donnie.dugger@sigmanest.com,816-454-0600,KS:MO,RSR:N&amp;ST
+        ///Jim Elmore,jim.elmore@sigmanest.com,513-214-9269,CA (S):NV(S):AZ,RSR:SW
+        ///Faber Fields,faber.fields@sigmanest.com,513-430-2973,IN (S),RSR:GL
+        ///Jenn [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string SalesReps {
             get {
