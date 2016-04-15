37ab7f8347bc8fdbefec65c3ccd563dcca634f63
derekantrican <derekantrican@gmail.com>
2016-04-14 23:22:33 -0700

    Set the "Signature Workaround" to be off by default

--- SalesMap/Properties/Settings.Designer.cs
+++ SalesMap/Properties/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace SalesMap.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool SignatureWorkaround {
             get {
                 return ((bool)(this["SignatureWorkaround"]));
