21ce22dbd96cd1470301867e004ebfff125ced19
derekantrican <derekantrican@gmail.com>
2016-09-29 12:21:56 -0700

    Removed unnecessary resource

--- SalesMap/Properties/Resources.Designer.cs
+++ SalesMap/Properties/Resources.Designer.cs
@@ -103,16 +103,6 @@ namespace SalesMap.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap offSMRPic {
-            get {
-                object obj = ResourceManager.GetObject("offSMRPic", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap preview {
             get {
                 object obj = ResourceManager.GetObject("preview", resourceCulture);
